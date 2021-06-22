c10e3a57c85237c24875f3b7b359d9333aab3d01
Anirup Patnaik <anirup049@gmail.com>
2021-06-23 04:29:05 +0530

    Side Nav component creation

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,13 @@
 import React from "react";
 import classes from "./Home.module.css";
+import SideNav from "../SideNav/SideNav";
 
 const Home = () => {
   return (
     <div className={classes.home}>
-      <div className={classes.home__left}></div>
+      <div className={classes.home__left}>
+        <SideNav />
+      </div>
       <div className={classes.home__right}></div>
     </div>
   );
---
+++ src/components/SideNav/SideNav.js
@@ -0,0 +1,13 @@
+import classes from "./SideNav.module.css";
+
+import React from "react";
+
+const SideNav = () => {
+  return (
+    <div className={classes.sidenav}>
+      <p>Side nav bar</p>
+    </div>
+  );
+};
+
+export default SideNav;
