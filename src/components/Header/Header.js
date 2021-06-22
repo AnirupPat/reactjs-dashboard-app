835c8b0da0e564a2e463be15e8458055feb8f411
Anirup Patnaik <anirup049@gmail.com>
2021-06-23 04:23:04 +0530

    Header Component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,7 +2,18 @@ import React from "react";
 import classes from "./Header.module.css";
 
 const Header = () => {
-  return <div className={classes.header}></div>;
+  return (
+    <div className={classes.header}>
+      <div className={classes.header__leftSection}>
+        <span className={classes.mainLogo}>Accenture</span>
+        <span className={classes.tagText}>High Performance Delivered.</span>
+      </div>
+
+      <div className={classes.header__rightSection}>
+        <h2>Workstream Dashboard</h2>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
