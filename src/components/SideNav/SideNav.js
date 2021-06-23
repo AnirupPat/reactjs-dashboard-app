8cc374a904127a7fef677d7f20114d7ce37b7cc2
Anirup Patnaik <anirup049@gmail.com>
2021-06-24 03:46:13 +0530

    Minor CSS correction

--- src/components/SideNav/SideNav.js
+++ src/components/SideNav/SideNav.js
@@ -4,11 +4,13 @@ import NavigationList from "../NavigationList/NavigationList";
 import React from "react";
 const SAP_FIORI = "SAP FIORI";
 const SAP_IA = "INTELLIGENT AUTOMATION";
+const SAP_ABAP = "SAP ABAP";
 const SideNav = () => {
   return (
     <div className={classes.sidenav}>
       <NavigationList projectTag={SAP_FIORI} params={["Stories", "Revenues"]} />
       <NavigationList projectTag={SAP_IA} params={["Stories", "Revenues"]} />
+      <NavigationList projectTag={SAP_ABAP} params={["Stories", "Revenues"]} />
     </div>
   );
 };
